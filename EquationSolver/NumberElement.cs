847d69f2b21aca4c99335a9f0822f3fca93bed9e
Jacques Lucke <mail@jlucke.com>
2014-07-20 20:44:21 +0200

    can parse numbers and plus symbol

---
+++ EquationSolver/NumberElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationSolver
+{
+    public class NumberElement : IElement
+    {
+        private double number = 0;
+        public NumberElement(double number)
+        {
+            this.number = number;
+        }
+
+        public double Number
+        {
+            get { return number; }
+        }
+
+        public override string ToString()
+        {
+            return "" + number;
+        }
+    }
+}
--- EquationSolver/Program.cs
+++ EquationSolver/Program.cs
@@ -11,6 +11,7 @@ namespace EquationSolver
         static void Main(string[] args)
         {
             string text = GetInputString();
+            List<IElement> elements = GetElementsFromString(text);
             Console.ReadLine();
         }
 
@@ -18,6 +19,43 @@ namespace EquationSolver
         {
             Console.Write("Term: ");
             return Console.ReadLine();
+        }
+        private static List<IElement> GetElementsFromString(string text)
+        {
+            List<IElement> elements = new List<IElement>();
+            while (text.Length > 0)
+            {
+                elements.Add(FindAndDeleteFirstElement(ref text));
+            }
+            return elements;
+        }
+        private static IElement FindAndDeleteFirstElement(ref string text)
+        {
+            // multiple chars elements
+            if(Char.IsDigit(text[0]))
+            {
+                NumberElement element = GetFirstNumberElement(text);
+                text = text.Substring(element.ToString().Length);
+                return element;
+            }
+            // single char elements
+            if(text[0] == '+')
+            {
+                PlusElement element = new PlusElement();
+                text = text.Substring(1);
+                return element;
+            }
+            return null;
+        }
+        private static NumberElement GetFirstNumberElement(string text)
+        {
+            int endIndex = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsDigit(text[i])) endIndex = i;
+                else break;
+            }
+            return new NumberElement(Convert.ToDouble(text.Substring(0, endIndex + 1)));
         }
     }
 }
