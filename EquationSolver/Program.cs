627bae6f7e3d30aa8b8427e9fcf99c76e269f4e9
Jacques Lucke <mail@jlucke.com>
2014-07-21 15:54:05 +0200

    output result

--- EquationSolver/Program.cs
+++ EquationSolver/Program.cs
@@ -18,7 +18,7 @@ namespace EquationSolver
                 TermParser parser = new TermParser(text);
                 parser.Parse();
                 elements = parser.Elements;
-                parser.TopLayer.Calculate(null);
+                Console.WriteLine(parser.TopLayer.Calculate(null));
             }
             catch (ParseStringException e)
             {
