ce28c625d65f5fba4d5070e9a3b0a79f406ee58d
Jacques Lucke <mail@jlucke.com>
2014-07-24 21:33:03 +0200

    change test output

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -42,8 +42,11 @@ namespace TestProject
             testEquations.Add(new KeyValuePair<string, double>("root_3_(x - 3) = 3", 30));
 
             bool failed = false;
-            foreach(KeyValuePair<string, double> pair in testEquations)
-                if (!TestEquation(pair.Key, pair.Value)) failed = true;
+            foreach (KeyValuePair<string, double> pair in testEquations)
+            {
+                if (!TestEquation(pair.Key, pair.Value)) { failed = true; Console.ReadKey(); }
+                Console.WriteLine();
+            }
 
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
@@ -54,7 +57,7 @@ namespace TestProject
                 Console.ReadKey();
             }
             else
-                Console.WriteLine("test ok");            
+                Console.WriteLine("test ok");
         }
 
         static bool TestEquation(string equationString, double answer)
@@ -64,13 +67,18 @@ namespace TestProject
             if (Utils.GetOptimizedTextFromNumber(e.Terms[1].Calculate()) == Utils.GetOptimizedTextFromNumber(answer))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("correct   " + equationString + "                        x = " + e.Terms[1].ToString());
+                Console.WriteLine("correct");
+                Console.WriteLine(equationString);
+                Console.WriteLine("x = " + e.Terms[1].ToString());
                 return true;
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("not correct    " + equationString + "             answer: x = " + e.Terms[1].ToString() + "   correct: x = " + Utils.GetOptimizedTextFromNumber(answer));
+                Console.WriteLine("not correct");
+                Console.WriteLine(equationString);
+                Console.WriteLine("x = " + e.Terms[1].ToString());
+                Console.WriteLine("answer: " + Utils.GetOptimizedTextFromNumber(answer));
                 return false;
             }
         }
