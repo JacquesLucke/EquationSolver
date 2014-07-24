e943058d1e10090131a61dc78469c07eec7c16c2
Jacques Lucke <mail@jlucke.com>
2014-07-24 13:50:58 +0200

    cleanup

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -13,23 +13,24 @@ namespace TestProject
         {
             List<KeyValuePair<string, double>> testEquations = new List<KeyValuePair<string, double>>();
 
-            testEquations.Add(new KeyValuePair<string, double>("x+3=10", 7));
-            testEquations.Add(new KeyValuePair<string, double>("10+x=20", 10));
-            testEquations.Add(new KeyValuePair<string, double>("2+3=x+2", 3));
-            testEquations.Add(new KeyValuePair<string, double>("19+x+3=22", 0));
-            testEquations.Add(new KeyValuePair<string, double>("88-3+2=x+1", 86));
+            testEquations.Add(new KeyValuePair<string, double>("x + 3 = 10", 7));
+            testEquations.Add(new KeyValuePair<string, double>("10 + x = 20", 10));
+            testEquations.Add(new KeyValuePair<string, double>("2 + 3 = x + 2", 3));
+            testEquations.Add(new KeyValuePair<string, double>("19 + x + 3 = 22", 0));
+            testEquations.Add(new KeyValuePair<string, double>("88 - 3 + 2 = x + 1", 86));
 
-            testEquations.Add(new KeyValuePair<string, double>("5x+2*3=11", 1));
-            testEquations.Add(new KeyValuePair<string, double>("(-3)*2+8=2x", 1));
-            testEquations.Add(new KeyValuePair<string, double>("8x+2*4=2x", -4/(double)3));
-            testEquations.Add(new KeyValuePair<string, double>("8*2+10x=8x-2", -9));
-            testEquations.Add(new KeyValuePair<string, double>("6/(3x)=10", 1/(double)5));
+            testEquations.Add(new KeyValuePair<string, double>("5x + 2*3 = 11", 1));
+            testEquations.Add(new KeyValuePair<string, double>("(-3)*2 + 8 = 2x", 1));
+            testEquations.Add(new KeyValuePair<string, double>("8x + 2*4 = 2x", -4/(double)3));
+            testEquations.Add(new KeyValuePair<string, double>("8*2 + 10x = 8x - 2", -9));
+            testEquations.Add(new KeyValuePair<string, double>("6/(3x) = 10", 1/(double)5));
 
-            testEquations.Add(new KeyValuePair<string, double>("2x=14", 7));
-            testEquations.Add(new KeyValuePair<string, double>("8-3x=-1", 3));
-            testEquations.Add(new KeyValuePair<string, double>("17-4x=1-12x", -2));
-            testEquations.Add(new KeyValuePair<string, double>("3(x-2)=5(x-4)", 7));
-            testEquations.Add(new KeyValuePair<string, double>("25-(17-2x)=2+(8x-6)", 2));
+            testEquations.Add(new KeyValuePair<string, double>("2x = 14", 7));
+            testEquations.Add(new KeyValuePair<string, double>("8 - 3x = -1", 3));
+            testEquations.Add(new KeyValuePair<string, double>("17 - 4x = 1 - 12x", -2));
+            testEquations.Add(new KeyValuePair<string, double>("3(x - 2) = 5(x - 4)", 7));
+            testEquations.Add(new KeyValuePair<string, double>("25 - (17 - 2x) = 2 + (8x - 6)", 2));
+            testEquations.Add(new KeyValuePair<string, double>("2(x - 1) = (3x + 5)/2", 9));
 
             bool failed = false;
             foreach(KeyValuePair<string, double> pair in testEquations)
@@ -53,13 +54,13 @@ namespace TestProject
             if (Utils.GetOptimizedTextFromNumber(e.Terms[1].Calculate()) == Utils.GetOptimizedTextFromNumber(answer))
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("correct   " + equationString + "   x = " + e.Terms[1].ToString());
+                Console.WriteLine("correct   " + equationString + "                        x = " + e.Terms[1].ToString());
                 return true;
             }
             else
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("not correct    " + equationString + "  answer: x = " + e.Terms[1].ToString() + "   correct: x = " + Utils.GetOptimizedTextFromNumber(answer));
+                Console.WriteLine("not correct    " + equationString + "             answer: x = " + e.Terms[1].ToString() + "   correct: x = " + Utils.GetOptimizedTextFromNumber(answer));
                 return false;
             }
         }
